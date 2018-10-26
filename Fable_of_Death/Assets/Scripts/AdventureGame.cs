4eb87c7c597d4a91518e9034e669cfcbd0b3ec46
Dreaddisk <joey_manuel@outlook.com>
2018-10-26 12:01:42 -0400

    3. Implementing states

---
+++ Fable_of_Death/Assets/Scripts/AdventureGame.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+    #region Variables
+    [SerializeField] Text textComponent;
+    #endregion
+
+    #region UnityFunctions
+    private void Start()
+    {
+        textComponent.text = ("You are into my domain");
+    }
+    #endregion
+}// Main Class
